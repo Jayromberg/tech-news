3a1a23cea455262ae2d7f649393f569870d699e2
Jayromberg <jayro.dev@gmail.com>
2023-02-13 11:54:28 -0300

    feat: Menu concluido, porém com erro de complexidade

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,8 +1,14 @@
 import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_categories
 
 
-# Requisitos 11 e 12
-def analyzer_menu():
+def menu():
     print(
         "Selecione uma das opções a seguir:\n"
         " 0 - Popular o banco com notícias;\n"
@@ -12,24 +18,37 @@ def analyzer_menu():
         " 4 - Listar top 5 categorias;\n"
         " 5 - Sair."
     )
+
+
+# Requisitos 11 e 12
+def analyzer_menu():
+    menu()
     option = input()
 
-    if option == 0:
+    if option == "0":
         print("Digite quantas notícias serão buscadas:")
         quantity = input()
-    elif option == 1:
+        a = get_tech_news(int(quantity))
+        print(a)
+    elif option == "1":
         print("Digite o título:")
         title = input()
-    elif option == 2:
+        b = search_by_title(title)
+        print(b)
+    elif option == "2":
         print("Digite a data no formato aaaa-mm-dd:")
         date = input()
-    elif option == 3:
+        c = search_by_date(date)
+        print(c)
+    elif option == "3":
         print("Digite a categoria:")
         category = input()
-    elif option == 4:
-        print("")
-    elif option == 5:
+        d = search_by_category(category)
+        print(d)
+    elif option == "4":
+        e = top_5_categories()
+        print(e)
+    elif option == "5":
         print("Encerrando script\n")
-        sys.exit()
     else:
         print("Opção inválida", file=sys.stderr)
