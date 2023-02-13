77e66f9b57ed1265927b1443158f6cd150158573
Jayromberg <jayro.dev@gmail.com>
2023-02-13 10:50:30 -0300

    feat: top_5_categories concluido

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,13 @@
+from collections import Counter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    rank_categories = Counter(map(
+        lambda x: x['category'],
+        news)
+    ).most_common(5)
+    sort_category = sorted(rank_categories, key=lambda x: (-x[1], x[0]))
+    return [new[0] for new in sort_category]
